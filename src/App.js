4f02044f376d7130eeda9a92adfb96825e3416d7
vandu1979 <vandana_kothari@yahoo.com>
2023-01-26 13:43:56 -0700

    frontend setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import pic from './images/pic.jfif'
+import useStyles from './styles';
 
-function App() {
+export default function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Container maxwidth="lg">
+    <AppBar className={classes.appBar} position="static" color="inherit">
+   
+   <Typography className={classes.heading}
+   variant="h2" align="center">Activate Works</Typography>
+    <img className={classes.image} src={pic} alt="pic" height="60" />
+    </AppBar>
+    <Grow in>
+        <Container>
+            <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
+                <Grid item xs={12} sm={7}>
+                  <Posts />
+                    {/* <Posts setCurrentId={setCurrentId}/> */}
+                    </Grid>
+                    
+                
+                <Grid item xs={12} sm={4}>
+                    {/* <Form currentId={currentId} setCurrentId={setCurrentId} /> */}
+                    <Form />
+                    </Grid>
+            </Grid>
+        </Container>
 
-export default App;
+    </Grow>
+</Container>
+    )
+}
+ 
---
+++ src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import useStyles from './styles';
+
+export default function Form() {
+  const classes = useStyles();
+  return (
+    <div>
+        <h1>Form</h1>
+    </div>
+  )
+}
